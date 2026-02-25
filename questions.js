ac976f746aaab7ccada147602e1bc8b94e2e6725
aitaissaf030-art <aitaissaf030@gmail.com>
2026-02-26 01:25:33 +0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,56 @@
+const questionBank = {
 
+medical: [
+{
+q: "What is the normal body temperature of a healthy adult?",
+o: ["35°C", "36°C", "37°C", "38°C"],
+a: 2
+},
+{
+q: "How many chambers does the human heart have?",
+o: ["2", "3", "4", "5"],
+a: 2
+},
+{
+q: "Which organ produces insulin?",
+o: ["Liver", "Pancreas", "Kidney", "Heart"],
+a: 1
+},
+{
+q: "What blood type is known as universal donor?",
+o: ["A", "B", "AB", "O negative"],
+a: 3
+},
+{
+q: "Normal human blood pH is?",
+o: ["6.8", "7.4", "8.0", "5.5"],
+a: 1
+},
+{
+q: "Which vitamin is produced by sunlight?",
+o: ["Vitamin A", "Vitamin B", "Vitamin C", "Vitamin D"],
+a: 3
+},
+{
+q: "Largest organ in human body?",
+o: ["Heart", "Liver", "Skin", "Brain"],
+a: 2
+},
+{
+q: "Which mineral is important for bones?",
+o: ["Iron", "Calcium", "Sodium", "Potassium"],
+a: 1
+},
+{
+q: "Normal adult pulse rate?",
+o: ["40-60", "60-100", "100-120", "120-140"],
+a: 1
+},
+{
+q: "Which disease is caused by insulin deficiency?",
+o: ["Cancer", "Diabetes", "Asthma", "Malaria"],
+a: 1
+}
+]
+
+};
